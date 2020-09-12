664682697d61760d309239c651af0f1a69d49a54
AdorkablyNicole <hello@nicolegeorge.me>
2020-09-12 13:23:29 +0930

    case studies rendering on homepage

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -23,26 +23,28 @@ get_header(); ?>
 			$image_1 = get_field('image_1');
 			$image_2 = get_field('image_2');
 			$image_3 = get_field('image_3'); ?>
+
+			
 		<article class="case-study">
 			<aside class="case-study-sidebar">
 				<h2><?php the_title(); ?></h2>
 				<h5><?php echo $services; ?></h5>
-				<h6><?php echo $client; ?></h6>
+				<h6>Client: <?php echo $client; ?></h6>
 				
 				<?php the_content(); ?>
 
-				<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+				<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site</a></strong></p>
 			</aside>
 			
 			<div class="case-study-images">
 				<?php if ($image_1) { ?>
-					<img src="<?php echo $image_1; ?>" />
+					<img src="<?php echo $image_1?>" />
 				<?php } ?>
 
 				<?php if ($image_2) { ?>
 					<img src="<?php echo $image_2; ?>" />
 				<?php } ?>
-				
+
 				<?php if (image_3) { ?>
 				<img src="<?php echo $image_3; ?>" />
 				<?php } ?>
